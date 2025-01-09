601c1bf89fbb7a0c8ac94b6bdbc14ae97202a20c
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2025-01-09 15:13:36 -0800

    Fixed display sleep calcuation results

--- src/components/SleepResults/SleepResults.vue
+++ src/components/SleepResults/SleepResults.vue
@@ -1,26 +1,29 @@
 <template>
-  <div>
+  <div class="space-y-6">
     <div class="text-center space-y-2">
-      <h1 class="text-3xl font-bold text-primary-300">Results</h1>
-      <h2 class="text-lg font-medium text-white">Your time is saved</h2>
+      <h1 class="text-3xl font-bold text-primary-300">Fall asleep at...</h1>
+      <p>Remember, the average human takes around 15 minutes to fall asleep, so
+        please plan accordingly. You will thank me later.</p>
+    </div>
+    <div class="flex justify-center gap-4 text-primary-300 font-medium text-sm">
+      <span>&#x1F504; Sleep Cycles</span>
+      <span>&#x1F551; Hours of Sleep</span>
     </div>
     <div class="flex flex-col items-center justify-center gap-4">
       <div>
-        <p class="text-2xl font-bold">{{ formattedTime }}</p>
-      </div>
-      <div>
         <div class="space-y-4">
           <template v-for="(result, index) in sleepTimes" :key="index">
-            <SleepTime :time="result.time" :cycles="result.cycles" :hours="result.duration / 60" />
+            <SleepTime :time="result.time" :cycles="result.cycles"
+              :hours="result.duration / 60" />
           </template>
         </div>
       </div>
-      <div>
-        <button @click="reset"
-          class="rounded-xl p-2 px-8 bg-primary-300 text-primary-950 font-medium transition-transform transform hover:bg-opacity-90 active:scale-95">
-          Calculate Again
-        </button>
-      </div>
+    </div>
+    <div class="text-center">
+      <button @click="reset"
+        class="rounded-xl p-2 px-8 bg-primary-300 text-primary-950 font-medium transition-transform transform hover:bg-opacity-90 active:scale-95">
+        Calculate Again
+      </button>
     </div>
   </div>
 </template>
@@ -42,11 +45,6 @@ export default {
   },
   components: {
     SleepTime,
-  },
-  computed: {
-    formattedTime() {
-      return formatTime(store.time);
-    },
   },
   methods: {
     reset,
--- src/components/SleepResults/SleepTime.vue
+++ src/components/SleepResults/SleepTime.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <h1>{{ formatTime(time) }}</h1>
-    <div class="flex gap-4">
-      <h6>Cycles: {{ cycles }}</h6>
-      <h6>Duration: {{ hours }}</h6>
+  <div class="border-2 border-white/40 bg-black/5 p-4 rounded-xl space-y-3">
+    <div class="flex align-center gap-2">
+      <h1 class="font-bold text-3xl text-primary-300">{{ formatTime(time) }}</h1>
+      <div class="space-x-2 flex items-center text-sm">
+        <span>&#x1F504; {{ cycles }}</span>
+        <span>&#x1F551; {{ hours }}</span>
+      </div>
+    </div>
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
   </div>
 </template>
